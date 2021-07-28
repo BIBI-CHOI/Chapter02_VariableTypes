1f05dd819d3c7f318f8a1113c15a00831a03e778
bitcamp <bitcamp@DESKTOP-U94PCCM>
2021-07-28 20:52:57 +0900

    2

---
+++ src/ch02_2_primitive/LongExample.java
@@ -0,0 +1,16 @@
+package ch02_2_primitive;
+
+public class LongExample {
+	public static void main(String[] args) {
+		long var1 = 10;
+		long var2 = 20L;
+//		long var3 = 10000000000; long의 범위를 넘어서 오류
+		long var4 = 100000000L;
+		
+		System.out.println(var1);
+		System.out.println(var2);
+//		System.out.println(var3);
+		System.out.println(var4);
+	}
+
+}
