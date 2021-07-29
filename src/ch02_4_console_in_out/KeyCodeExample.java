da7af6f50ad2bde9704d61ec508f41a5b0082e7b
User <User@DESKTOP-20GJ8HB>
2021-07-30 00:26:10 +0900

    2

--- src/ch02_4_console_in_out/KeyCodeExample.java
+++ src/ch02_4_console_in_out/KeyCodeExample.java
@@ -1,9 +1,16 @@
 package ch02_4_console_in_out;
 
 public class KeyCodeExample {
-	public static void main(String[] args) throw Exception {
+	public static void main(String[] args) throws Exception {
+		int keyCode;
 		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
 		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
+		
+		keyCode = System.in.read();
+		System.out.println("keyCode: " + keyCode);
 	}
-
 }
